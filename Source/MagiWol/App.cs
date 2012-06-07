f9ede651400210dcc3fef147975999856a303401
Josip Medved <jmedved@jmedved.com>
2012-06-06 22:07:25 -0700

    Fixing setup mutex.

--- Source/MagiWol/App.cs
+++ Source/MagiWol/App.cs
@@ -1,40 +1,42 @@
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Principal;
 using System.Threading;
 using System.Windows.Forms;
 
 namespace MagiWol {
     internal static class App {
 
-        private static Mutex _setupMutex;
-
-
         [STAThread]
         static void Main() {
-            _setupMutex = new Mutex(false, @"Global\JosipMedved_MagiWOL");
+            bool createdNew;
+            var mutexSecurity = new MutexSecurity();
+            mutexSecurity.AddAccessRule(new MutexAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), MutexRights.FullControl, AccessControlType.Allow));
+            using (var setupMutex = new Mutex(false, @"Global\JosipMedved_MagiWOL", out createdNew, mutexSecurity)) {
 
-            System.Windows.Forms.Application.EnableVisualStyles();
-            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
-            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(Medo.Configuration.Settings.Read("CultureName", "en-US"));
+                System.Windows.Forms.Application.EnableVisualStyles();
+                System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+                System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(Medo.Configuration.Settings.Read("CultureName", "en-US"));
 
-            Medo.Application.UnhandledCatch.ThreadException += new EventHandler<ThreadExceptionEventArgs>(UnhandledCatch_ThreadException);
-            Medo.Application.UnhandledCatch.Attach();
+                Medo.Application.UnhandledCatch.ThreadException += new EventHandler<ThreadExceptionEventArgs>(UnhandledCatch_ThreadException);
+                Medo.Application.UnhandledCatch.Attach();
 
-            Medo.Configuration.Settings.NoRegistryWrites = !Settings.IsInstalled;
-            Medo.Configuration.RecentFiles.NoRegistryWrites = !Settings.IsInstalled;
-            Medo.Windows.Forms.State.NoRegistryWrites = !Settings.IsInstalled;
-            Medo.Diagnostics.ErrorReport.DisableAutomaticSaveToTemp = !Settings.IsInstalled;
+                Medo.Configuration.Settings.NoRegistryWrites = !Settings.IsInstalled;
+                Medo.Configuration.RecentFiles.NoRegistryWrites = !Settings.IsInstalled;
+                Medo.Windows.Forms.State.NoRegistryWrites = !Settings.IsInstalled;
+                Medo.Diagnostics.ErrorReport.DisableAutomaticSaveToTemp = !Settings.IsInstalled;
 
-            if (!((Environment.OSVersion.Version.Build < 7000) || (App.IsRunningOnMono))) {
-                var appId = Assembly.GetExecutingAssembly().Location;
-                if (appId.Length > 127) { appId = @"JosipMedved_MagiWOL\" + appId.Substring(appId.Length - 127 - 20); }
-                NativeMethods.SetCurrentProcessExplicitAppUserModelID(appId);
+                if (!((Environment.OSVersion.Version.Build < 7000) || (App.IsRunningOnMono))) {
+                    var appId = Assembly.GetExecutingAssembly().Location;
+                    if (appId.Length > 127) { appId = @"JosipMedved_MagiWOL\" + appId.Substring(appId.Length - 127 - 20); }
+                    NativeMethods.SetCurrentProcessExplicitAppUserModelID(appId);
+                }
+
+                Application.Run(new MainForm());
+
             }
-
-            Application.Run(new MainForm());
-
-            _setupMutex.Close();
         }
 
 
